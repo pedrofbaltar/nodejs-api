b38d44f4d61ee45875dde890dc264272f9f3044c
Pedro Felipe Baltar <pfbaltariead@gmail.com>
2021-02-23 09:57:27 -0300

    Configuring the database

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,18 @@
 import express from 'express';
+import { router } from './routes';
+import 'reflect-metadata';
+import './database'
 
 const app = express();
 
-app.listen(3000, () => console.log('Server is running!'));
+app.use(express.json());
+app.use(router);
+app.listen(3000, () => console.log("Server is running!"));
+
+/**
+ * GET
+ * POST
+ * PUT
+ * DELETE
+ * PATCH => Alteração específica
+ */
